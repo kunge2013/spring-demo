d27efc6016b2fdd3eee9e283464b98538293adce
fangkun <panzhangyi@squrab.com>
2020-04-26 16:12:52 +0800

    1

--- src/main/java/org/kframe/springioc/Application.java
+++ src/main/java/org/kframe/springioc/Application.java
@@ -8,6 +8,8 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
 		IDbService dbService = context.getBean(IDbService.class);
+		IDbService dbService2 = context.getBean(IDbService.class);
+		System.out.println("dbService hashcode ==" + dbService.hashCode() + ", dbService2 ===" + dbService2.hashCode());
 		dbService.save();
 	}
 }
--- src/main/java/org/kframe/springioc/DbService.java
+++ src/main/java/org/kframe/springioc/DbService.java
@@ -2,8 +2,18 @@ package org.kframe.springioc;
 
 public class DbService implements IDbService {
 
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void save() {
-		System.out.println("save method ......");
+		System.out.println("save method ......" + this.name);
 	}
 	
 }
