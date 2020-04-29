5e9d71080af66d39e4be0e93c0df4c19851a4a6a
fangkun <panzhangyi@squrab.com>
2020-04-29 09:47:11 +0800

    1

--- src/main/java/org/kframe/springioc/annotations/AnnotationApplication.java
+++ src/main/java/org/kframe/springioc/annotations/AnnotationApplication.java
@@ -1,11 +1,26 @@
 package org.kframe.springioc.annotations;
 
+import java.util.HashMap;
+
+import org.kframe.springioc.annotations.Config.ConfigInfo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AnnotationApplication {
+	
+	private static AnnotationConfigApplicationContext context;
+
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("org.kframe.springioc.annotations");
+		
+		context = new AnnotationConfigApplicationContext("org.kframe.springioc.annotations");
 		Config config = context.getBean(Config.class);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("1", "hehe");
+		context.getBeanFactory().registerSingleton("hello", map);
+		ConfigInfo configInfo = context.getBean(Config.ConfigInfo.class);
+		System.out.println(configInfo.toString());
 		System.out.println(config.toString());
+		System.out.println(context.getBean("hello"));
+		
 	}
+	
 }
--- src/main/java/org/kframe/springioc/annotations/Config.java
+++ src/main/java/org/kframe/springioc/annotations/Config.java
@@ -1,5 +1,6 @@
 package org.kframe.springioc.annotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,5 +13,40 @@ public class Config {
 		return "Config [name=" + name + "]";
 	}
 	
+	
+	@Bean
+	public ConfigInfo configInfo() {
+		return new ConfigInfo();
+	}
+
+	class ConfigInfo {
+		
+		private String name = "xx";
+		
+		private int age = 10;
+		
+		public ConfigInfo() {
+			// TODO Auto-generated constructor stub
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+		@Override
+		public String toString() {
+			return "ConfigInfo [name=" + name + ", age=" + age + "]";
+		}
+		
+		
+	}
 	
 }
