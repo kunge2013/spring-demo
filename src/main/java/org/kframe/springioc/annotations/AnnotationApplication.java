13ae723219dd960fbcb74eadea5cc5275d1f1a2d
fangkun <panzhangyi@squrab.com>
2020-04-29 14:42:59 +0800

    12

--- src/main/java/org/kframe/springioc/annotations/AnnotationApplication.java
+++ src/main/java/org/kframe/springioc/annotations/AnnotationApplication.java
@@ -1,5 +1,6 @@
 package org.kframe.springioc.annotations;
 
+import org.kframe.springioc.annotations.aop.ArithmeticCalculator;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AnnotationApplication {
@@ -9,8 +10,10 @@ public class AnnotationApplication {
 	public static void main(String[] args) throws Exception {
 		System.out.println("=======================begin  AnnotationConfigApplicationContext=======================================");
 		context = new AnnotationConfigApplicationContext("org.kframe.springioc.annotations");
-		System.out.println("String==== 代码手动注入后的String ：" +  context.getBean("String"));
-		System.out.println("config==== name 被改编后的值 ：" +  context.getBean(Config.class).getName());
+		ArithmeticCalculator calc = context.getBean(ArithmeticCalculator.class);
+		System.out.println(calc.add(1, 2));
+//		System.out.println("String==== 代码手动注入后的String ：" +  context.getBean("String"));
+//		System.out.println("config==== name 被改编后的值 ：" +  context.getBean(Config.class).getName());
 	}
 	
 }
