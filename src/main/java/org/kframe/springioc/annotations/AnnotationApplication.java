f43cdae489772683c0e4a590c98566ef246e38ee
fangkun <panzhangyi@squrab.com>
2020-05-06 10:05:13 +0800

    1

--- src/main/java/org/kframe/springioc/annotations/AnnotationApplication.java
+++ src/main/java/org/kframe/springioc/annotations/AnnotationApplication.java
@@ -11,6 +11,9 @@ public class AnnotationApplication {
 		System.out.println("=======================begin  AnnotationConfigApplicationContext=======================================");
 		context = new AnnotationConfigApplicationContext("org.kframe.springioc.annotations");
 		ArithmeticCalculator calc = context.getBean(ArithmeticCalculator.class);
+		FactoryBeanInstance o = (FactoryBeanInstance) context.getBean("&factoryBeanInstance");
+		Object hash = context.getBean("factoryBeanInstance");
+		System.out.println(o.hashCode() + hash.toString());
 		System.out.println(calc.add(1, 2));
 //		System.out.println("String==== 代码手动注入后的String ：" +  context.getBean("String"));
 //		System.out.println("config==== name 被改编后的值 ：" +  context.getBean(Config.class).getName());
